1b912fa655c5cf5f6aba28c07f7235024618c66d
Pathum Madhusanka <pathum@centricnode.com>
2026-02-18 23:59:55 +0530

    feat: add bac button to profile page

--- src/pages/ProfileView/ProfileViewPage.tsx
+++ src/pages/ProfileView/ProfileViewPage.tsx
@@ -1,10 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 
 export default function ProfileView() {
 	const { id } = useParams();
+	const navigate = useNavigate();
 
 	return (
 		<>
+			<Button
+				onClick={() => navigate(-1)}
+				variant="outline"
+				className="mb-4 cursor-pointer"
+			>
+				<ChevronLeft className="h-4 w-4 mr-2" />
+				All Profiles
+			</Button>
 			<p>Profile ID: {id}</p>
 		</>
 	);
