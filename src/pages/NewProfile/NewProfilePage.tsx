1d093c12bdcfdddf66385185fb03d29a80e31dd6
Pathum Madhusanka <pathum@centricnode.com>
2026-02-21 14:01:46 +0530

    feat: improve UI

--- src/pages/NewProfile/NewProfilePage.tsx
+++ src/pages/NewProfile/NewProfilePage.tsx
@@ -72,7 +72,7 @@ export default function NewProfile() {
 
 	return (
 		<>
-			<div className="flex items-center justify-between mb-4">
+			{/* <div className="flex items-center justify-between mb-4">
 				<Button
 					onClick={() => navigate(-1)}
 					variant="outline"
@@ -81,128 +81,134 @@ export default function NewProfile() {
 					<ChevronLeft className="h-4 w-4 mr-2" />
 					All Profiles
 				</Button>
-			</div>
+			</div> */}
 
-			<div className="max-w-[680px] mx-auto">
+			<div className="w-full mx-auto">
+				<span className="text-lg font-medium whitespace-nowrap">
+					Create New Profile
+				</span>
+
+				<Separator className="mt-2 mb-10" />
 				<form onSubmit={handleSubmit}>
-					<div className="flex flex-col gap-6">
-						<span className="text-lg font-medium whitespace-nowrap">
-							Create New Profile
-						</span>
-						{/* <Separator /> */}
-
-						<div className="grid gap-2">
-							<Label htmlFor="fullName">Full Name</Label>
-							<Input
-								id="fullName"
-								type="text"
-								placeholder="John Doe"
-								required
-								value={fullName}
-								onChange={(e) => setFullName(e.target.value)}
-							/>
-						</div>
-						<div className="grid gap-2">
-							<Label htmlFor="nic">NIC</Label>
-							<Input
-								id="nic"
-								type="text"
-								placeholder="Optional (must be unique)"
-								value={nic}
-								onChange={(e) => setNic(e.target.value)}
-							/>
-						</div>
-						<div className="grid gap-2">
-							<Label htmlFor="alias">Alias</Label>
-							<Input
-								id="alias"
-								type="text"
-								placeholder="Optional"
-								value={alias}
-								onChange={(e) => setAlias(e.target.value)}
-							/>
-						</div>
-
-						<div className="mt-2">
+					<div className="max-w-[680px] mx-auto">
+						<div className=" w-full flex flex-col gap-6">
 							<div className="grid gap-2">
-								<Label htmlFor="addressLine1">
-									Address Line 1
-								</Label>
+								<Label htmlFor="fullName">Full Name</Label>
 								<Input
-									id="addressLine1"
+									id="fullName"
 									type="text"
-									placeholder="Optional"
-									value={addressLine1}
+									placeholder="John Doe"
+									required
+									value={fullName}
 									onChange={(e) =>
-										setAddressLine1(e.target.value)
+										setFullName(e.target.value)
 									}
 								/>
 							</div>
-							<div className="grid gap-2 mt-6">
-								<Label htmlFor="addressLine2">
-									Address Line 2
-								</Label>
+							<div className="grid gap-2">
+								<Label htmlFor="nic">NIC</Label>
 								<Input
-									id="addressLine2"
+									id="nic"
 									type="text"
-									placeholder="Optional"
-									value={addressLine2}
-									onChange={(e) =>
-										setAddressLine2(e.target.value)
-									}
+									placeholder="Optional (must be unique)"
+									value={nic}
+									onChange={(e) => setNic(e.target.value)}
 								/>
 							</div>
-							<div className="grid gap-2 mt-6">
-								<Label htmlFor="city">City</Label>
+							<div className="grid gap-2">
+								<Label htmlFor="alias">Alias</Label>
 								<Input
-									id="city"
+									id="alias"
 									type="text"
 									placeholder="Optional"
-									value={city}
-									onChange={(e) => setCity(e.target.value)}
+									value={alias}
+									onChange={(e) => setAlias(e.target.value)}
 								/>
 							</div>
-						</div>
 
-						<div className="grid gap-2">
-							<Label htmlFor="notes">Notes</Label>
-							<Textarea
-								maxLength={500}
-								id="notes"
-								placeholder="Include notes"
-								value={notes}
-								onChange={(e) => setNotes(e.target.value)}
-							/>
-							<div className="text-sm text-gray-500">
-								{notes.length}/500
+							<div className="mt-2">
+								<div className="grid gap-2">
+									<Label htmlFor="addressLine1">
+										Address Line 1
+									</Label>
+									<Input
+										id="addressLine1"
+										type="text"
+										placeholder="Optional"
+										value={addressLine1}
+										onChange={(e) =>
+											setAddressLine1(e.target.value)
+										}
+									/>
+								</div>
+								<div className="grid gap-2 mt-6">
+									<Label htmlFor="addressLine2">
+										Address Line 2
+									</Label>
+									<Input
+										id="addressLine2"
+										type="text"
+										placeholder="Optional"
+										value={addressLine2}
+										onChange={(e) =>
+											setAddressLine2(e.target.value)
+										}
+									/>
+								</div>
+								<div className="grid gap-2 mt-6">
+									<Label htmlFor="city">City</Label>
+									<Input
+										id="city"
+										type="text"
+										placeholder="Optional"
+										value={city}
+										onChange={(e) =>
+											setCity(e.target.value)
+										}
+									/>
+								</div>
 							</div>
-						</div>
 
-						<div className="flex justify-between mt-6 pb-10 gap-2">
-							<Button
-								type="button"
-								variant="outline"
-								className="cursor-pointer"
-								onClick={clearFields}
-							>
-								Clear
-							</Button>
+							<div className="grid gap-2">
+								<Label htmlFor="notes">Notes</Label>
+								<Textarea
+									maxLength={500}
+									id="notes"
+									placeholder="Include notes"
+									value={notes}
+									onChange={(e) => setNotes(e.target.value)}
+								/>
+								<div className="text-sm text-gray-500">
+									{notes.length}/500
+								</div>
+							</div>
 
-							<div className="flex gap-2">
-								<Button
-									type="submit"
-									variant="default"
-									className="cursor-pointer"
-								>
-									Save Profile
-								</Button>
+							<div className="flex justify-between mt-6 pb-10 gap-2">
 								<Button
 									type="button"
-									variant="default"
+									variant="outline"
 									className="cursor-pointer"
+									onClick={clearFields}
 								>
-									Save & File Cases
+									Clear
 								</Button>
+
+								<div className="flex gap-2">
+									<Button
+										type="submit"
+										variant="default"
+										className="cursor-pointer"
+									>
+										Save Profile
+									</Button>
+									<Button
+										type="button"
+										variant="default"
+										className="cursor-pointer"
+									>
+										Save & File Cases
+									</Button>
+								</div>
 							</div>
 						</div>
 					</div>
