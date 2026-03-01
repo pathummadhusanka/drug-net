80317ad2a973cfb0f62532cef048b694cde11178
Pathum Madhusanka <pathum@centricnode.com>
2026-03-01 19:41:56 +0530

    improve ui

--- src/pages/ProfileView/ProfileViewPage.tsx
+++ src/pages/ProfileView/ProfileViewPage.tsx
@@ -1079,7 +1079,7 @@ export default function ProfileView() {
 															placeholder="Optional"
 														/>
 													</div>
-													<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+													<div className="grid grid-cols-1 gap-4">
 														<div className="grid gap-2">
 															<Label htmlFor="edit-address-line-1">
 																Address Line 1
